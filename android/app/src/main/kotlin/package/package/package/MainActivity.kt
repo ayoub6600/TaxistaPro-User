06d33c779a56a3294fc0fa1ccb66d1a29d24c180
ahmed <am7405782@gmail.com>
2025-02-04 21:42:56 +0200

    firest

---
+++ android/app/src/main/kotlin/package/package/package/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.taxista.user
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:taxista/firebase_options.dart';
+import 'functions/functions.dart';
+import 'functions/notifications.dart';
+import 'pages/loadingPage/loadingpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  checkInternetConnection();
+  initMessaging();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    platform = Theme.of(context).platform;
+    return GestureDetector(
+        onTap: () {
+          //remove keyboard on touching anywhere on the screen.
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+            FocusManager.instance.primaryFocus?.unfocus();
+          }
+        },
+        child: ValueListenableBuilder(
+            valueListenable: valueNotifierBook.value,
+            builder: (context, value, child) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Taxista',
+                theme: ThemeData(),
+                home: const LoadingPage(),
+                navigatorObservers: [BotToastNavigatorObserver()],
+                builder: (context, widget) {
+                  Function botToast = BotToastInit();
+                  Widget mWidget = botToast(context, widget);
+                  return MediaQuery(
+                    //Setting font does not change with system font size
+                    data: MediaQuery.of(context)
+                        .copyWith(textScaler: const TextScaler.linear(1.0)),
+                    child: mWidget,
+                  );
+                },
+              );
+            }));
+  }
+}
